69cb809e28413a0cedc0160fb6fc6e3be1d6a75d
unknown <osamashreba@hotmail.com>
2018-02-20 10:50:29 -0800

    add class atm

--- atm_solution/atm.py
+++ atm_solution/atm.py
@@ -1,28 +1,40 @@
+class Atm():
+ #def bull(balance,request):
+ def __init__(self,balance,bank_name):
+  self.balance=balance
+  self.bank_name=bank_name
+ def bull(self,request):
+   self.request=request
 
-def bull(balance,request):
- list_count=[100,50,10,5,4,3,2,1]
+   list_count=[100,50,10,5,4,3,2,1]
+   print 'welcom to'+' '+ 'bank'+ ' '+self.bank_name
 
- if balance>=request:
-	for amount in list_count:
-		while request>=amount:
-			print 'give',amount
+   if self.balance>=request:
+	  for amount in list_count:
+		  while request>=amount:
+			  print 'give',amount
 		
 			
-			request -=amount
-			balance -=amount
+			  request -=amount
+			  self.balance -=amount
 
-
-	print 'you have now'+' '+(str (balance))+'$'
-	return balance
+	  print 'you have now'+' '+(str (self.balance))+'$'
+	  return self.balance
 
  		    			
- print 'you do not have enough mony'+"  " +'you have'+' '+str(balance)+'$'
-balance=500
-balance=bull(balance,277)
-balance=bull(balance,30)
-balance=bull(balance,5)
-balance=bull(balance,188)
-balance=bull(balance,110)
+   print 'you do not have enough mony'+"  " +'you have'+' '+str(self.balance)+'$'
+atm1=Atm(500,'baraka')
+atm2=Atm(200,'Alshark')
+atm1.bull(277)
+atm2.bull(209)
+
+
+#balance=500
+#balance=bull(balance,277)
+#balance=bull(balance,30)
+#balance=bull(balance,5)
+#balance=bull(balance,188)
+#balance=bull(balance,110)
 
 
 
